7e86686acbc61277764a6f11210b9c2ef0833ab6
Morgan King <mking4@trinity.edu>
2018-09-14 12:44:59 -0500

    complete

--- bucket.h
+++ bucket.h
@@ -1,28 +1,55 @@
-//Team C: Daniel Matthewson, Madeline Wilson, Morgan King
-//CSCI 3322: Principles of Algorithms
-//Problem Set Three: Bucket Sort
-//September 17th, 2017
-//Pledged
-
-#include <iostream>
-#include <algorithm>
+#pragma once
+#include <iterator>
+#include <math.h>
 #include <vector>
+#include <stdio.h>
+#include<algorithm>
 
 using namespace std;
 
-template<typename T,typename K>
+template<typename T, typename K> void bucketSort(T begin, T end, K keyFunc);
+int bucket(double val, int length, double max);
+
+template<typename T, typename K>
 void bucketSort(T begin, T end, K keyFunc) {
-  //create n buckets
-  vector
+	T itr = begin + 1;
 
-  //put array elements in different buckets
-  for (T round = begin; round != end-1; round++) {
-    auto elem = *round;
+	//find max value in array for bucket formula
+	double max = keyFunc(*begin);
+	while (itr < end) {
+		if (keyFunc(*itr) > max) {
+			max = keyFunc(*itr);
+		}
+		itr = itr + 1;
+	}
 
-  }
+	int numBuckets = end - begin;
+	vector < vector<typename iterator_traits<T>::value_type >> buckets(numBuckets);
+	itr = begin;
 
-  //sort individual bucketSort using insertion sort
+	//add elements to buckets
+	while (itr < end) {
+		buckets[bucket(keyFunc(*itr), numBuckets, max)].push_back(*itr);
+		itr = itr + 1;
+	}
 
-  //concatenate all buckets into original array
+	//sort individual buckets
+	for (auto &v : buckets) {
+		stable_sort(v.begin(), v.end(), [keyFunc](typename iterator_traits<T>::value_type a, typename iterator_traits<T>::value_type b) { return keyFunc(a) < keyFunc(b); });
+	}
 
+	//return elements to vector in order
+	itr = begin;
+	for (auto &v : buckets) {
+		for (auto i : v) {
+			*itr = i;
+			itr = itr + 1;
+		}
+	}
+}
+
+
+//find bucket where value belongs
+int bucket(double val, int length, double max) {
+	return floor((val * length) / (max + 1));
 }
